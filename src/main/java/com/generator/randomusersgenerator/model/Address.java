5220b7ab4bc181efebc9f0bb77db2edca11eddac
Banji <87107632+Banji-07@users.noreply.github.com>
2022-12-05 03:00:40 +0100

    Used the @NotNull and @Size on all fields except the id to ensure/provide validation.

--- src/main/java/com/generator/randomusersgenerator/model/Address.java
+++ src/main/java/com/generator/randomusersgenerator/model/Address.java
@@ -2,35 +2,49 @@ package com.generator.randomusersgenerator.model;
 
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
-@SuppressWarnings("JpaObjectClassSignatureInspection")
 @Data
 @Getter
+@Setter
 @ToString
 public class Address {
     @Id
     @GeneratedValue
-    private Long id;
+    private Long addressId;
+    @NotNull
+    @Size(min = 5)
     private String streetAddress;
+    @NotNull
+    @Size(min = 4)
     private String zipCode;
-    private String City;
-    private String State;
-    private String Country;
+    @NotNull
+    @Size(min = 5)
+    private String city;
+    @NotNull
+    @Size(min = 3)
+    private String state;
+    @NotNull
+    @Size(min = 3)
+    private String country;
 
-    private Address() {
+    public Address() {
     }
+
 
     public Address(String streetAddress, String zipCode, String city, String state, String country) {
         this.streetAddress = streetAddress;
         this.zipCode = zipCode;
-        this.City = city;
-        this.State = state;
-        this.Country = country;
+        this.city = city;
+        this.state = state;
+        this.country = country;
     }
 }
