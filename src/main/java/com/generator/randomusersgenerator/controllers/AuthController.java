a14f32b8555a96e68fdcd2d26d95f86fc054a69c
Banji <87107632+Banji-07@users.noreply.github.com>
2022-12-05 03:13:10 +0100

    A try catch block was implemented to return the jwt with HttpStatus 202 if authentication is complete or catch any AuthenticationException thrown and return HttpStatus 401 is Unauthorized

--- src/main/java/com/generator/randomusersgenerator/controllers/AuthController.java
+++ src/main/java/com/generator/randomusersgenerator/controllers/AuthController.java
@@ -1,11 +1,14 @@
 package com.generator.randomusersgenerator.controllers;
 
-import com.paging.paging.model.LoginParam;
-import com.paging.paging.services.JwtTokenGenerator;
+import com.generator.randomusersgenerator.model.LoginParam;
+import com.generator.randomusersgenerator.services.JwtTokenGenerator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,9 +28,13 @@ public class AuthController {
     }
 
     @PostMapping(consumes = "application/json")
-    public String generateToken(@RequestBody LoginParam loginParam) {
-        Authentication authentication = authManager
-                .authenticate(new UsernamePasswordAuthenticationToken(loginParam.username(), loginParam.password()));
-        return jwtTokenGenerator.generateJWTTokens(authentication);
+    public ResponseEntity<String> generateToken(@RequestBody LoginParam loginParam) {
+        try {
+            Authentication authentication = authManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginParam.username(),loginParam.password()));
+            return ResponseEntity.accepted().body(jwtTokenGenerator.generateJWTTokens(authentication));
+        } catch (AuthenticationException authenticationException) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
     }
 }
