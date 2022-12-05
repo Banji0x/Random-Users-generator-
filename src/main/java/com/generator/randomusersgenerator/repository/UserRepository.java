cdfd537371c4f4587aecb6eb6751473c53d0cf3c
Banji <87107632+Banji-07@users.noreply.github.com>
2022-12-05 03:46:38 +0100

    extended JpaRepository instead of  PagingAndSortingRepository even though the methods provided by JpaRepository wasn't used.

--- src/main/java/com/generator/randomusersgenerator/repository/UserRepository.java
+++ src/main/java/com/generator/randomusersgenerator/repository/UserRepository.java
@@ -4,5 +4,5 @@ package com.generator.randomusersgenerator.repository;
 import com.generator.randomusersgenerator.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends PagingAndSortingRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long> {
 }
