1bb5d266059931d111723f1dbc10fc4ad1363b1a
Banji <87107632+Banji-07@users.noreply.github.com>
2022-12-05 04:01:30 +0100

    Intercepts the generateJWTTokens,commandLineRunner methods, and the SecurityLog method and logs their activities to the console.

--- src/main/java/com/generator/randomusersgenerator/aspects/LoggingAspect.java
+++ src/main/java/com/generator/randomusersgenerator/aspects/LoggingAspect.java
@@ -1,16 +1,17 @@
 package com.generator.randomusersgenerator.aspects;
 
-import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 
 @Aspect
-@Slf4j
 @Component
 public class LoggingAspect {
-    @Around("execution(* com.paging.paging.services.JwtTokenGenerator.generateJWTTokens(..))")
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Around("execution(* com.generator.randomusersgenerator.services.JwtTokenGenerator.generateJWTTokens(..))")
     public Object JwtTokenGenerator(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         log.info("The " + proceedingJoinPoint.getSignature().getName() + " method in " + proceedingJoinPoint.getSignature().getDeclaringTypeName() + " was invoked");
         Object jwtToken = proceedingJoinPoint.proceed();
@@ -18,14 +19,14 @@ public class LoggingAspect {
         return jwtToken;
     }
 
-    @Around("execution(* com.paging.paging.services.DataLoader.commandLineRunner(..))")
+    @Around("execution(* com.generator.randomusersgenerator.services.DataLoader.commandLineRunner(..))")
     public Object logAuthentication(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         Object commandLineRunner = proceedingJoinPoint.proceed();
         log.info("Fake details generated and added to database successfully");
         return commandLineRunner;
     }
 
-    @Around("@annotation(com.paging.paging.annotation.SecurityLog)")
+    @Around("@annotation(com.generator.randomusersgenerator.annotation.SecurityLog)")
     public Object cryptography(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         String name = proceedingJoinPoint.getSignature().getName();
         log.info(name + " bean created...");
