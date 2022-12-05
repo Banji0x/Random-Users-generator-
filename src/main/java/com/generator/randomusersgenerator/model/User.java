1365f9e59ea6a329446bdba9a10e9af5e8c1bbec
Banji <87107632+Banji-07@users.noreply.github.com>
2022-12-05 03:33:40 +0100

    Used annotations different annotations like @NotNull,@Email,@Size on fields except the id to ensure/provide validation.

--- src/main/java/com/generator/randomusersgenerator/model/User.java
+++ src/main/java/com/generator/randomusersgenerator/model/User.java
@@ -1,36 +1,48 @@
 package com.generator.randomusersgenerator.model;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
-@SuppressWarnings("JpaObjectClassSignatureInspection")
 @Entity
 @Table(name = "users")
 @ToString
 @Getter
+@Setter
 public class User {
 
     @Id
     @GeneratedValue
     private Long id;
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "address_id", referencedColumnName = "id")
-    private Address address;
+    @NotNull
+    @Size(min = 2)
     private String firstName;
+    @NotNull
+    @Size(min = 2)
     private String lastName;
+    @NotNull
+    @Size(max = 17)
     private String phoneNumber;
+    @Email
     private String emailAddress;
+    @NotNull
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "addressId")
+    private Address address;
 
-    private User(){
+    public User() {
     }
 
-    public User(String firstName, String lastName,String phoneNumber, String emailAddress, Address address) {
-        this.address = address;
+    public User(String firstName, String lastName, String phoneNumber, String emailAddress, Address address) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
         this.emailAddress = emailAddress;
+        this.address = address;
     }
 }
